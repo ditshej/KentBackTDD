7d6410461984210eac4fc865fb08bb0caddf435a
Raphi W <ditshej@gmail.com>
2021-08-28 16:54:42 +0200

    Chapter 4 Degenerate Objects - expand the multiplication test

--- tests/CalculationTest.php
+++ tests/CalculationTest.php
@@ -9,8 +9,10 @@ class CalculationTest extends TestCase {
 
 	public function testMultiplication() : void {
 		$five = new Dollar(5);
-		$five->times(2);
-		self::assertEquals(10, $five->amount);
+		$product = $five->times(2);
+		self::assertEquals(10, $product->amount);
+		$product = $five->times(3);
+		self::assertEquals(15, $product->amount);
 	}
 
 }
