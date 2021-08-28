2c2cb9ede70e1f918ac1ce257078342f35e12a9e
Raphi W <ditshej@gmail.com>
2021-08-28 16:37:11 +0200

    Chapter 3 Money Example - make the test for multiplication pass

---
+++ src/Dollar.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description.
+ */
+
+namespace App;
+
+class Dollar {
+
+	public int $amount = 10;
+
+	public function __construct(int $amount) {
+	}
+
+	public function times(int $multiplier) {
+
+	}
+
+}
--- tests/CalculationTest.php
+++ tests/CalculationTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests;
 
+use App\Dollar;
 use PHPUnit\Framework\TestCase;
 
 class CalculationTest extends TestCase {
+
 	public function testMultiplication() : void {
-		$five = new \Dollar(5);
-		$five.times(2);
-		self::assertEquals(10, $five.amount);
+		$five = new Dollar(5);
+		$five->times(2);
+		self::assertEquals(10, $five->amount);
 	}
+
 }
