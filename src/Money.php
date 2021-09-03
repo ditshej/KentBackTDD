bc9eee28136f7eb383929938a1d39a1492a46893
Raphi W <ditshej@gmail.com>
2021-09-03 10:15:52 +0200

    Chapter 9 Apples and Oranges - five Dollars are not equal to five Francs

--- src/Money.php
+++ src/Money.php
@@ -7,7 +7,8 @@ class Money {
 	protected int $amount;
 
 	public function equals(Money $money) : bool {
-		return $this->amount === $money->amount;
+		return $this->amount === $money->amount
+				&& get_class($this) === get_class($money);
 	}
 
 }
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -24,5 +24,6 @@ class MoneyTest extends TestCase {
 		self::assertFalse((new Dollar(5))->equals(new Dollar(6)));
 		self::assertTrue((new Franc(5))->equals(new Franc(5)));
 		self::assertFalse((new Franc(5))->equals(new Franc(6)));
+		self::assertFalse((new Franc(5))->equals(new Dollar(5)));
 	}
 }
